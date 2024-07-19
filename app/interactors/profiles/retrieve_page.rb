f972f55f6cc28dd45d217f7ea57d05d1c54f1aa4
André Galatti <andreluisgalatti@hotmail.com>
2024-07-19 10:57:41 -0300

    config: switch to firefox so it works on docker

--- app/interactors/profiles/retrieve_page.rb
+++ app/interactors/profiles/retrieve_page.rb
@@ -8,7 +8,7 @@ module Profiles
     include Interactor
 
     def call
-      browser = Watir::Browser.new(:chrome, headless: true, service: { port: "9999" })
+      browser = Watir::Browser.new(:firefox, headless: true, service: { port: "9999" })
       browser.goto(context.profile.github_url)
       browser.wait_until { browser.element(css: "div.js-yearly-contributions").exists? } # wait for turbo-frame
       context.page = Nokogiri::HTML.parse(browser.html)
